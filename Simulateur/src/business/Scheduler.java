016ece2b07a0649a60b0c91a865182f9b3843299
Claire <canneltigrou@gmail.com>
2018-09-30 22:41:32 +0200

    Ajout d'un mode
    Ajout d'un mode dynamic - à temps limité ou à l'infini

--- Simulateur/src/business/Scheduler.java
+++ Simulateur/src/business/Scheduler.java
@@ -21,6 +21,11 @@ public class Scheduler {
 	private static Proc processor;
 	private static ArrayList<IOenum> messageList;
 
+	// for choose the mode we want to run : dynamic mode, or until we stop the
+	// program (ctrl-C) or with a choosen time ?
+	private static boolean dynamic;
+	private static double chosenTime;
+
 	public static boolean askContinue() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("\nDo you want to continue the simulation ? (y|n)\n");
@@ -28,6 +33,60 @@ public class Scheduler {
 		if (s.equals("y") || s.equals("Y"))
 			return true;
 		return false;
+	}
+
+	public static void askMode() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean stop = false;
+		int chosenNumber = 0;
+		//double tmpTime = 0;
+		String s ;
+		while (!stop)
+		{
+			System.out.println("Choose a mode :");
+			System.out.println("  1 - Dynamic mode (you choose to continue or not at each incrementation of time)");
+			System.out.println("  2 - Infinite mode (you must stop manually the program to stop the simulation : ctrl-C)");
+			System.out.println("  3 - Time mode (you choose a time, and the simulation will work until this time)");
+			System.out.println("Your number : ");
+			
+			//choosenNumber = NumberUtils.toInt("1234");
+			
+			s = br.readLine();	
+			try {
+				chosenNumber = Integer.parseInt(s);
+				if (chosenNumber > 0 && chosenNumber < 4)
+					stop = true;
+			} catch (Exception e) {
+				System.out.println("Please, enter a number between 1 and 3 :");
+			}
+		}
+			
+		if (chosenNumber == 1)
+			dynamic = true;
+		else
+			dynamic = false;
+		
+		if(chosenNumber == 2)
+			chosenTime = Double.POSITIVE_INFINITY;
+		
+		if(chosenNumber == 3)
+		{
+			stop = false;
+			while (!stop)
+			{
+				System.out.println("How many time do you want to play ? :");
+				System.out.println("Your number : ");				
+				s = br.readLine();	
+				try {
+					chosenTime = Double.parseDouble(s);
+					stop = true;
+				} catch (Exception e) {
+					System.out.println("This number was not valid. Please, enter a valid number.");
+				}
+			}
+			
+		}
+		
 	}
 
 	private static AtomicComponent impactedComponent(IOenum message) {
@@ -46,14 +105,13 @@ public class Scheduler {
 			res.add(impactedComponent(io));
 		return res;
 	}
-	
-	
+
 	private static void initialisation_Scheduler() {
 		components = new ArrayList<>();
 		imminentComponents = new ArrayList<>();
 		impactedComponents = new ArrayList<>();
 		messageList = new ArrayList<>();
-		
+
 		generator = new Gen("Generateur");
 		components.add(generator);
 		buffer = new Buf("Buffer");
@@ -61,27 +119,30 @@ public class Scheduler {
 		processor = new Proc("Processeur");
 		components.add(processor);
 		stop = false;
-		
+
 	}
 
 	public static void main(String[] args) throws IOException {
-		
+
 		initialisation_Scheduler();
+		askMode();
+		
 		Double tmp;
 		ChartFrame cf = new ChartFrame("gbp", "GBP");
 		Chart qChart = new Chart("q");
-		//qChart.addDataToSeries(0.0, 1.0);
+		// qChart.addDataToSeries(0.0, 1.0);
 		cf.addToLineChartPane(qChart);
 		for (AtomicComponent c : components)
 			c.init();
 		temps = 0.d;
+		
 		while (!stop) {
 			// on recupere les differents Ta pour savoir le temps du prochain evenement et
 			// les composants a executer.
 			System.out.println("temps : " + temps.toString());
 			imminentComponents.clear();
 			tempsMin = components.get(0).getTa();
-			//tempsMin = Double.POSITIVE_INFINITY;	
+			// tempsMin = Double.POSITIVE_INFINITY;
 			for (AtomicComponent c : components) {
 				tmp = c.getTa();
 				System.out.println(c.name + ".Ta = " + tmp.toString());
@@ -93,15 +154,13 @@ public class Scheduler {
 					imminentComponents.add(c);
 			}
 			System.out.println("Le prochain evenement se produira dans t + " + tempsMin);
-			
 
 			// j'execute lambda et recupere les composants impactés
 			temps += tempsMin;
 			System.out.println(" Temps = " + temps);
-			
+
 			messageList.clear();
-			for (AtomicComponent c : imminentComponents)
-			{	
+			for (AtomicComponent c : imminentComponents) {
 				System.out.println(c.toString() + " execute Lambda.");
 				messageList.addAll(c.lambda());
 
@@ -120,14 +179,18 @@ public class Scheduler {
 					if (impactedComponents.contains(c))
 						c.delta_ext(messageList);
 					else
-						//temps += tempsMin;
+						// temps += tempsMin;
 						c.IncrementTime(tempsMin);
 				}
 			}
 			qChart.addDataToSeries(temps, buffer.getQ());
 			System.out.println("le q = " + buffer.getQ());
-			stop = !askContinue();
+			
+			if(dynamic)
+				stop = !askContinue();
+			else
+				if (temps > chosenTime)
+					stop = true;
 		}
-
 	}
 }
